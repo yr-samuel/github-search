802f9914d17d75006050608ebf918a4fb74a8d90
Samuel Araujo <samuel.aoliveira74@gmail.com>
2020-11-25 23:41:06 -0300

    Implementado loading

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,29 +1,42 @@
+const GITHUBACTION = {
+  SUCCESS: "GET_GITHUB_SUCCESS",
+  LOADING: "GET_GITHUB_LOADING",
+  FAIL: "GET_GITHUB_FAIL",
+};
+
 const INITIAL_STATE: IInialState = {
   users: [],
   fail: false,
   loading: false,
-  items: null,
   user: "",
+  totalCount: 0,
 };
 
 interface IInialState {
   users: any;
   fail: boolean;
   loading: boolean;
-  items: any;
   user: string;
+  totalCount: number;
 }
 
 export default function githubReducer(state = INITIAL_STATE, action: any) {
   switch (action.type) {
-    case "GET_GITHUB_SUCCESS":
+    case GITHUBACTION.SUCCESS:
       return {
         ...INITIAL_STATE,
         users: action.payload.users,
         user: action.payload.user,
+        totalCount: action.payload.totalCount,
       };
 
-    case "GET_GITHUB_FAIL":
+    case GITHUBACTION.LOADING:
+      return {
+        ...INITIAL_STATE,
+        loading: true,
+      };
+
+    case GITHUBACTION.FAIL:
       return {
         ...INITIAL_STATE,
         fail: true,
@@ -36,19 +49,22 @@ export default function githubReducer(state = INITIAL_STATE, action: any) {
 
 export function githubAction(user: any) {
   return async function (dispatch: any) {
+    dispatch({ type: GITHUBACTION.LOADING });
+
     try {
       const url = "https://api.github.com/search/users?q=";
       const response = await fetch(url + user);
       const jsonUsers = await response.json();
       dispatch({
-        type: "GET_GITHUB_SUCCESS",
+        type: GITHUBACTION.SUCCESS,
         payload: {
-          users: jsonUsers,
+          users: jsonUsers.items,
+          totalCount: jsonUsers.total_count,
           user,
         },
       });
     } catch (error) {
-      dispatch({ type: "GET_GITHUB_FAIL" });
+      dispatch({ type: GITHUBACTION.FAIL });
     }
   };
 }

27b90a6e0c09bc9d99e37793587cb90c0110c548
Samuel Araujo <samuel.aoliveira74@gmail.com>
2020-11-25 23:39:35 -0300

    Novo componente para loading

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./_loader.css";
+export default function Loader() {
+  return (
+    <div className="container">
+      <div className="loader"></div>
+    </div>
+  );
+}
